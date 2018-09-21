c9e41dc08be64d0da31d4123524dd9cfa7b8fd7d
David Falana <falanadavid@gmail.com>
2018-09-21 14:47:52 -0400

    Draft

--- src/ProblemSet2.java
+++ src/ProblemSet2.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class ProblemSet2 {
 	public static void main (String[] args) {
 		ProblemSet2 ps2 = new ProblemSet2();
@@ -9,11 +10,28 @@ public class ProblemSet2 {
 	}
 	
 	public void sayHello() {
+		System.out.println("Enter First and Last Name!");
+		Scanner in = new Scanner(System.in);
+		String text = in.nextLine();
+		System.out.println("Hello, " + text);
+		in.close();
 		
 	}
 	
 	public void gradeMe() {
-		
+		System.out.println("Enter Your Homework Grade!");
+		Scanner in = new Scanner(System.in);
+		String text2 = in.nextLine();
+		in.close();
+		System.out.println("Enter Your Quiz Grades!");
+		Scanner in = new Scanner(System.in);
+		String text3 = in.nextLine();
+		in.close();
+		System.out.println("Enter Your Test Grades!");
+		Scanner in = new Scanner(System.in);
+		String text4 = in.nextLine();
+		System.out.println("Hello, ");
+		in.close();
 	}
 	
 	public void groupUs() {
